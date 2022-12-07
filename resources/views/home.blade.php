e0e5f9c15a16d18b421f3ef47c037db5985c6fa9
Samuel Solomon <mrsamuelsolomon@gmail.com>
2022-12-07 18:09:10 +0000

    extending the master layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
-</head>
-<body>
+@extends('layout')
 
-    <div class="main">
-        <h1>Home Page</h1>
-        <p>This is the home page</p>
-    </div>
-    
-</body>
-</html>
+@section('title', 'Home')
+
+@section('content')
+    <h1>Home Page</h1>
+    <p>This is the home page</p>
+@endsection
