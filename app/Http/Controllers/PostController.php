82170b6e3af05d4d29cfb0062ebadc4bdaa2185e
Samuel Solomon <mrsamuelsolomon@gmail.com>
2022-12-08 10:40:13 +0000

    implementing the resource routes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,15 +6,6 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
 
     /**
      * Show the form for creating a new resource.
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,26 +20,28 @@ Route::get('/', [HomeController::class, 'home'])->name('home');
 
 Route::get('/about', [HomeController::class, 'about'])->name('about');
 
+Route::resource('posts', PostController::class)->except([
+    'index',
+]);
 
 
 
+// Route::name('posts.')->prefix('posts')->group(function () {
 
-Route::name('posts.')->prefix('posts')->group(function () {
+//     Route::get('/create', function () {
+//         return view('posts.create');
+//     })->name('create');
 
-    Route::get('/create', function () {
-        return view('posts.create');
-    })->name('create');
+//     Route::post('/', function (Request $request) {
+//         $request->validate([
+//             'title' => 'required',
+//             'description' => ['required', 'min:10'],
+//         ]);
 
-    Route::post('/', function (Request $request) {
-        $request->validate([
-            'title' => 'required',
-            'description' => ['required', 'min:10'],
-        ]);
-
-        return redirect()
-            ->route('posts.create')
-            ->with('success', 'Post is submitted! Title: ' .
-                $request->input('title') . ' Description: ' .
-                $request->input('description'));
-    })->name('store');
-});
+//         return redirect()
+//             ->route('posts.create')
+//             ->with('success', 'Post is submitted! Title: ' .
+//                 $request->input('title') . ' Description: ' .
+//                 $request->input('description'));
+//     })->name('store');
+// });
