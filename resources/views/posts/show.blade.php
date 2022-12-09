376ab008f66963d827f563c91e0c03a4f1fb8acb
Samuel Solomon <mrsamuelsolomon@gmail.com>
2022-12-09 23:44:28 +0000

    deleting posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,6 +8,14 @@
             <h1>{{ $post->title }}</h1>
             <p>{{ $post->description }}</p>
             <a href="{{ route('posts.edit', [$post]) }}">Edit Post</a>
+
+            <form method="POST" action="{{ route('posts.destroy', [$post]) }}">
+                @csrf
+                @method('DELETE')
+                <button class="delete" type="submit">
+                    Delete Post
+                </button>
+            </form>
         </div>
     </div>
 @endsection
