b4856cfca0b8610b13f197b7a5d5dc84613bdca6
Samuel Solomon <mrsamuelsolomon@gmail.com>
2022-12-09 23:02:51 +0000

    showing a specific post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -55,7 +55,11 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::findOrFail($id);
+
+        return view('posts.show', [
+            'post' => $post
+        ]);
     }
 
     /**
