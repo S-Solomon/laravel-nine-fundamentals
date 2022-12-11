9b8bf8166a1ca3298338899f7f2005abc3e09484
Samuel Solomon <mrsamuelsolomon@gmail.com>
2022-12-11 21:02:18 +0000

    implementing auth in blade templates

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,14 +7,21 @@
         <div class="post-content">
             <h1>{{ $post->title }}</h1>
             <p>{{ $post->description }}</p>
+
+            @can('update', $post)
             <a href="{{ route('posts.edit', [$post]) }}">Edit Post</a>
+            @endcan
 
             <form method="POST" action="{{ route('posts.destroy', [$post]) }}">
                 @csrf
                 @method('DELETE')
+
+                @can('delete', $post)
                 <button class="delete" type="submit">
                     Delete Post
                 </button>
+                @endcan
+                
             </form>
             <small>Posted by <b>{{ $post->user->name }}</b></small>
         </div>
