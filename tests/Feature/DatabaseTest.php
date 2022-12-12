1923f2f8bc5c784d849ddf47c0ea6b939bca6d10
Samuel Solomon <mrsamuelsolomon@gmail.com>
2022-12-12 08:39:38 +0000

    Testing models anf their relationship

--- tests/Feature/DatabaseTest.php
+++ tests/Feature/DatabaseTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Post;
+use App\Models\User;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -18,5 +19,18 @@ class DatabaseTest extends TestCase
         Post::factory()->count(3)->create();
 
         $this->assertDatabaseCount('posts', 3);
+    }
+
+    public function test_the_posts_user_relationship()
+    {
+        $user = User::factory()
+            ->hasPosts(5)
+            ->create();
+
+        $this->assertDatabaseCount('posts', 5);    
+        $this->assertDatabaseCount('users', 1);
+        $this->assertDatabaseHas('posts', [
+            'user_id' => $user->id,
+        ]);    
     }
 }
